4dc2f8caaaf791f8dd55d6b481ff56cd71734660
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-09-06 16:02:49 +0300

    stage commit

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import { Weather } from "./Weather/Weather";
 import { City } from "./Weather/interfaces/City";
 import { createCity } from "./Weather/utils/createCity";
 
-const samara: City = createCity({
-  name: "Samara",
-  latitude: 53.20007,
-  longitude: 50.15,
-  altitude: 117,
+const kharkiv: City = createCity({
+  name: "Kharkiv",
+  latitude: 49.98081,
+  longitude: 36.25272,
+  altitude: 152,
 });
 
-const weatherOfSamara: Weather = new Weather().of(samara).renderIn("#calendar");
-console.log(weatherOfSamara);
+new Weather().of(kharkiv).renderIn("#weather");
